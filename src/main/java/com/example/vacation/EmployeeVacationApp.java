8bac2605b5a11992b06d2b7aaf6432faea743d22
Devyani Sonwane <47854961+Devyani3310@users.noreply.github.com>
2025-04-09 15:45:25 +0530

    Create EmployeeVacationApp.java

---
+++ src/main/java/com/example/vacation/EmployeeVacationApp.java
@@ -0,0 +1,11 @@
+package com.example.vacation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeVacationApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeVacationApp.class, args);
+    }
+}
